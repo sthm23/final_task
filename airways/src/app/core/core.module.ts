9b24cf2d335ee7cb200029203e9b7754de6ef5f1
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-04-25 19:32:59 +0500

    feat: add auth modal to register and login

--- airways/src/app/core/core.module.ts
+++ airways/src/app/core/core.module.ts
@@ -7,8 +7,15 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-
-
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -16,12 +23,22 @@ import { MatMenuModule } from '@angular/material/menu';
     HeaderComponent,
     FooterComponent,
     ErrorComponent,
+    AuthModalComponent,
   ],
   imports: [
     CommonModule,
     MatIconModule,
     MatMenuModule,
+    MatDialogModule,
     CoreRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatButtonToggleModule,
+    MatSelectModule
+
   ]
 })
 export class CoreModule { }
