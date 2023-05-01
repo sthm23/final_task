89c645fdb15a3cdb71b5c91986026c3f511908d1
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-05-01 19:32:32 +0500

    feat: add macket carcas with information

--- airways/src/app/booking/pages/order/order.component.ts
+++ airways/src/app/booking/pages/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { airplane_icon, airport_Icon } from './icon';
 
 @Component({
   selector: 'app-order',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
 
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    // Note that we provide the icon here as a string literal here due to a limitation in
+    // Stackblitz. If you want to provide the icon from a URL, you can use:
+    // `iconRegistry.addSvgIcon('thumbs-up', sanitizer.bypassSecurityTrustResourceUrl('icon.svg'));`
+    iconRegistry.addSvgIconLiteral('airplane_icon', sanitizer.bypassSecurityTrustHtml(airplane_icon));
+    iconRegistry.addSvgIconLiteral('airport_Icon', sanitizer.bypassSecurityTrustHtml(airport_Icon));
+  }
 }
