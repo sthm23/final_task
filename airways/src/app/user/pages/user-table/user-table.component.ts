7f5aa9e39199e152bd671770d228aa59a1b8c531
baxromxoja16 <aglamof@mail.ru>
2023-05-21 14:42:32 +0500

    feat: add user-table and user-booking pages

--- airways/src/app/user/pages/user-table/user-table.component.ts
+++ airways/src/app/user/pages/user-table/user-table.component.ts
@@ -1,8 +1,33 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+export interface CartInfo {
+  id: number,
+  flightNumber: string,
+  destination: {
+    from: string,
+    return: string,
+  },
+  flightType: string,
+  departureDate: {
+    from: string,
+    return: string,
+  },
+  arrivalDate: {
+    from: Date,
+    return: Date,
+  },
+  passengerAmount: {
+    adults: number,
+    child: number,
+    infant: number
+  },
+  price: number,
+}
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
@@ -13,34 +38,43 @@ export class UserTableComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['No.', 'Flight', 'Type trip', 'Data & Time', 'Passengers', 'Price', 'btn'];
   dataSource = new MatTableDataSource();
   selection = new SelectionModel<any>(true, []);
+  commonPrice = 0;
+  getData!: CartInfo[];
 
   @ViewChild(MatSort) sort!: MatSort;
 
+  constructor(private userService: UserService, private router: Router){}
+
   ngOnInit(): void {
-    this.dataSource.data = [
-      {
-        id:1,
-        number: ['FR 1925'],
-        flight: ['Dublin - Warsaw', 'Moulin - Warsaw'],
-        date: ['1 Mar, 2023, 8:40-12:00', '18 Mar, 2023, 7:40-11:00'],
-        passengers: [{person: 'Adult', count: 1}, {person: 'Child', count: 1},{person: 'Infant', count: 1},],
-        price: 551,
-      },
-      {
-        id:2,
-        number: ['FR 1396'],
-        flight: ['Gdansk - Warsaw'],
-        date: ['28 Mar, 2023, 15:40-16:40'],
-        passengers: [{person: 'Adult', count: 1}, {person: 'Child', count: 0},{person: 'Infant', count: 0},],
-        price: 20.96,
-      },
-    ];
+    this.dataSource.data = this.userService.getData;
     for (let i = 0; i < this.dataSource.data.length; i++) {
-      this.selection.toggle(this.dataSource.data[i])
+      this.selection.toggle(this.dataSource.data[i]);
+      this.commonPrice += (this.dataSource.data[i] as CartInfo).price;
     }
+  }
+
+  changeCheckBox(e: MatCheckboxChange, element: CartInfo) {
+    e ? this.selection.toggle(element) : null;
+
+    const checkedElem = this.selection.selected.find((item) => item.id === element.id);
+
+    e.checked ? this.commonPrice += checkedElem.price : this.commonPrice -= element.price
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+  }
+
+  moreFlight(element: CartInfo) {
+    console.log(element);
+    this.router.navigate(['/user/user-booking']);
+  }
+
+  deleteItem(element: CartInfo){
+    console.log(element);
+  }
+
+  editItem(element: CartInfo){
+    console.log(element);
   }
 }
--- airways/src/app/user/user.module.ts
+++ airways/src/app/user/user.module.ts
@@ -5,6 +5,9 @@ import { UserComponent } from './pages/user/user.component';
 import { RouterModule } from '@angular/router';
 import { UserHomeComponent } from './pages/user-home/user-home.component';
 import { UserTableComponent } from './pages/user-table/user-table.component';
+import { ConvertHourPipe } from './pipes/convert-hour.pipe';
+import { UserBookingComponent } from './pages/user-booking/user-booking.component';
+import { SummaryCardComponent } from './components/summary-card/summary-card.component';
 
 
 
@@ -13,13 +16,20 @@ import { UserTableComponent } from './pages/user-table/user-table.component';
     UserComponent,
     UserHomeComponent,
     UserTableComponent,
+    ConvertHourPipe,
+    UserBookingComponent,
+    SummaryCardComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule.forChild([{
-      path: '', component: UserHomeComponent
-    }])
+    RouterModule.forChild([
+      {
+        path: '', component: UserHomeComponent
+      },
+      {
+        path: 'user-booking', component: UserBookingComponent
+      }])
   ]
 })
 export class UserModule { }
